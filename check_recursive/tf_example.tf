bb4b578765de330634a03308269143872b40d21a
Jim Barker <jim.barker@icsltd.co.uk>
2020-10-07 14:00:02 +0100

    testing tf format check recursivity

---
+++ check_recursive/tf_example.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 0.13.3"
+
+  required_providers {
+    aws = {
+      version = ">= 3.8.0"
+        source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-a4b950c3"
+  instance_type = "t2.micro"
+}
