3ac9c143381c079baffcbd85d0a72f7c5d65341f
Jim Barker <jim.barker@icsltd.co.uk>
2020-10-08 09:46:27 +0100

    remove known-bad server now tflint is working, and remove superfluous root check

--- example_terraform.tf
+++ example_terraform.tf
@@ -22,8 +22,3 @@ resource "aws_instance" "example" {
     Owner     = "Test Project"
   }
 }
-
-resource "aws_instance" "foo" {
-  ami           = "ami-0ff8a91507f77f867"
-  instance_type = "t1.2xlarge" # invalid type!
-}
